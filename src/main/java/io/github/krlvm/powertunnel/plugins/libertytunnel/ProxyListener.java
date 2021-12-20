58ef658ee5060244c7407744a0db5df0c1265385
krlvm <51774833+krlvm@users.noreply.github.com>
2021-12-20 19:23:00 +0300

    Fix copyright headers

--- src/main/java/io/github/krlvm/powertunnel/plugins/libertytunnel/ProxyListener.java
+++ src/main/java/io/github/krlvm/powertunnel/plugins/libertytunnel/ProxyListener.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PowerTunnel.  If not, see <https://www.gnu.org/licenses/>.
+ * along with LibertyTunnel.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package io.github.krlvm.powertunnel.plugins.libertytunnel;
